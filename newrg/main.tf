2058b57867e1ec34987852d6cbb825be6c3c90cd
jaiprakash <jaiprakash53@gmail.com>
2024-02-08 23:12:05 +0530

    commit

---
+++ newrg/main.tf
@@ -0,0 +1,24 @@
+resource "azurerm_resource_group" "resourcegroup" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.resourcegroup.name
+  location                 = azurerm_resource_group.resourcegroup.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_virtual_network" "example" {
+  depends_on          = [azurerm_storage_account.example]
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+}
